72c9bbb2cd21a8ade874f210541f93102f162fc2
Wiktor Jezierski <wiktor.jezierski@interia.pl>
2015-11-11 20:59:39 +0100

    testy autoincrementu

---
+++ BazyDanych2Projekt/src/main/java/DataBase/DataBaseController.java
@@ -0,0 +1,31 @@
+package DataBase;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.sql.Date;
+
+/**
+ * Created by user on 2015-11-11.
+ */
+public class DataBaseController {
+
+    public static void main(String[] args) {
+
+        WypozyczeniaEntity wyp = new WypozyczeniaEntity();
+        wyp.setIdEgzemplarza("7");
+        wyp.setPeselKlienta("93101400538");
+        wyp.setDataWyp(new Date(2015,10,15));
+        wyp.setIdFaktury("100");
+        wyp.setPeselPrac("93101400538");
+        wyp.setPeselKlienta("941313");
+
+
+        //zapis
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("NewPersistenceUnit");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(wyp);
+        entityManager.getTransaction().commit();
+    }
+}
