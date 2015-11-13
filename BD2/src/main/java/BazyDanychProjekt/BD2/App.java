dac116290566107a15bd0d8ad651492e73c25e4d
Wiktor Jezierski <wiktor.jezierski@interia.pl>
2015-11-13 21:43:01 +0100

    ECLIPSE

---
+++ BD2/src/main/java/BazyDanychProjekt/BD2/App.java
@@ -0,0 +1,35 @@
+package BazyDanychProjekt.BD2;
+
+import java.util.Date;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	WypozyczeniaEntity wyp = new WypozyczeniaEntity();
+        wyp.setIdEgzemplarza("7");
+        wyp.setPeselKlienta("93101400538");
+        wyp.setDataWyp(new Date(2015,10,10));
+        wyp.setIdFaktury("100");
+        wyp.setPeselPrac("00000000");
+        wyp.setPeselKlienta("000003");
+
+        //zapis
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("BD2");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(wyp);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+}
---
+++ BD2/src/main/java/BazyDanychProjekt/BD2/WypozyczeniaEntity.java
@@ -0,0 +1,88 @@
+package BazyDanychProjekt.BD2;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+
+
+/**
+ * The persistent class for the Wypozyczenia database table.
+ * 
+ */
+@Entity
+@Table(name="Wypozyczenia")
+@NamedQuery(name="WypozyczeniaEntity.findAll", query="SELECT w FROM WypozyczeniaEntity w")
+public class WypozyczeniaEntity implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private int id;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name="DATA_WYP")
+	private Date dataWyp;
+
+	@Column(name="ID_EGZEMPLARZA")
+	private String idEgzemplarza;
+
+	@Column(name="ID_FAKTURY")
+	private String idFaktury;
+
+	@Column(name="PESEL_KLIENTA")
+	private String peselKlienta;
+
+	@Column(name="PESEL_PRAC")
+	private String peselPrac;
+
+	public WypozyczeniaEntity() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getDataWyp() {
+		return this.dataWyp;
+	}
+
+	public void setDataWyp(Date dataWyp) {
+		this.dataWyp = dataWyp;
+	}
+
+	public String getIdEgzemplarza() {
+		return this.idEgzemplarza;
+	}
+
+	public void setIdEgzemplarza(String idEgzemplarza) {
+		this.idEgzemplarza = idEgzemplarza;
+	}
+
+	public String getIdFaktury() {
+		return this.idFaktury;
+	}
+
+	public void setIdFaktury(String idFaktury) {
+		this.idFaktury = idFaktury;
+	}
+
+	public String getPeselKlienta() {
+		return this.peselKlienta;
+	}
+
+	public void setPeselKlienta(String peselKlienta) {
+		this.peselKlienta = peselKlienta;
+	}
+
+	public String getPeselPrac() {
+		return this.peselPrac;
+	}
+
+	public void setPeselPrac(String peselPrac) {
+		this.peselPrac = peselPrac;
+	}
+
+}
