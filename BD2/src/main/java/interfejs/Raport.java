35a6d03a482219ccfe2b51bbbca5447542cd2bfe
Wiktor Jezierski <wiktor.jezierski@interia.pl>
2016-01-09 21:51:34 +0100

    roboczy

--- BD2/src/main/java/interfejs/Raport.java
+++ BD2/src/main/java/interfejs/Raport.java
@@ -10,20 +10,36 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PiePlot;
 import org.jfree.data.general.DefaultPieDataset;
 import org.jfree.data.general.PieDataset;
+
+import BazyDanychProjekt.BD2.DataBaseController;
+import BazyDanychProjekt.BD2.SprzetEntity;
+import BazyDanychProjekt.BD2.WypozyczeniaArchiwumEntity;
+import BazyDanychProjekt.BD2.WypozyczeniaEntity;
+
 import javax.swing.JComboBox;
 import javax.swing.JButton;
 import javax.swing.DefaultComboBoxModel;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.awt.event.ActionEvent;
 
 public class Raport extends JPanel {
 
-	
+	boolean x = false;
+	private JPanel Y;
+	private JPanel raport;
+	private DataBaseController mController;
 	
 	/**
 	 * Create the panel.
 	 */
 	public Raport() {
 		setLayout(null);
-		
+		Y = this;
+		mController = new DataBaseController();
 		JPanel panel = new JPanel();
 		panel.setBounds(10, 11, 430, 40);
 		add(panel);
@@ -35,14 +51,42 @@ public class Raport extends JPanel {
 		panel.add(comboBox);
 		
 		JButton btnNewButton = new JButton("Pokaż");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//select count(w.id), k.nazwisko from Wypozyczenia w inner join Klienci k ON w.pesel_klienta = k.pesel group by k.nazwisko;
+//				List<Object> lista = (List<Object>)mController.executeQuery("select count(k.pesel), k.nazwisko from KlienciEntity k group by k.nazwisko");
+//				List<Object> lista2 = (List<Object>)mController.executeQuery("select w.id, w.klienci.pesel from WypozyczeniaEntity w group by w.klienci.pesel");
+				
+				ArrayList<String> nazwiska = new ArrayList<>();
+				ArrayList<Long> ilosci = new ArrayList<>();
+				List<Object> nazwiskaList = (List<Object>)mController.executeQuery("select k.nazwisko, count(w.id) from WypozyczeniaEntity w, KlienciEntity k where w.klienci.pesel = k.pesel group by k.nazwisko");
+				List<Object> ilosciList = (List<Object>)mController.executeQuery("select count(w.id) from WypozyczeniaEntity w, KlienciEntity k where w.klienci.pesel = k.pesel group by k.nazwisko");
+				
+				for (Object o : nazwiskaList) {
+					nazwiska.add((String)o);
+				}
+				for (Object o : ilosciList) {
+					ilosci.add((long)o);
+				}
+				
+				raport = createDemoPanel();
+				raport.setBounds(10, 60, 600, 500);
+				add(raport);
+				
+//				if(!x){
+//					remove(raport);
+//					repaint();
+//					x=!x; 
+//				}
+//				else{
+//					add(raport);
+//					repaint();
+//					x=!x;
+//				}
+			}
+		});
 		btnNewButton.setBounds(237, 10, 89, 23);
 		panel.add(btnNewButton);
-		
-		JPanel raport = createDemoPanel();
-		raport.setBounds(10, 60, 600, 500);
-		add(raport);
-		
-
 	}
 
 	private static PieDataset createDataset() {
